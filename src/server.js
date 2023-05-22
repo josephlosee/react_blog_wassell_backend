655968722cdaa7a73c11dbeb6e78cd0a1f5484c6
Joe Losee <josephlosee@gmail.com>
2023-05-22 15:44:35 -0700

    Ch2.4 - Completed added basic URL parameter example

--- src/server.js
+++ src/server.js
@@ -2,12 +2,21 @@ import express from "express";
 const listeningPort = 8000;
 
 const app = express();
+
 app.use(express.json()); // Middleware!
+
+
 
 app.post('/hello', (req, res)=>{
     console.log(req.body);
     res.send(`Hello, ${req.body.name}!`);
 });
+
+app.get('/hello/:name', (req, res) => {
+    // const name = req.params.name;
+    const {name} = req.params;
+    res.send(`Hello, ${name}, how odd to include it as a URL Parameter!`);
+} )
 
 app.listen(listeningPort, () => {
     console.log(`Server is listening on port ${listeningPort}`);
