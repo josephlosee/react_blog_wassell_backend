7e6a001373ff6aa723b761f1ad0bfeebb0392da0
Joe Losee <josephlosee@gmail.com>
2023-05-22 16:18:05 -0700

    Chapter 2 Completed - Added basic commenting for in-memory db

--- src/server.js
+++ src/server.js
@@ -4,14 +4,17 @@ let articlesInfo = [
     {
         name: 'learn-react',
         upvotes: 0,
+        comments: [],
     },
     {
         name: 'learn-node',
         upvotes: 0,
+        comments: [],
     },
     {
         name: 'mongodb',
         upvotes: 0,
+        comments: [],
     },
 ]
 
@@ -32,7 +35,22 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     }
         
     
-})
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const {name} = req.params;
+    const {postedBy, text} = req.body;
+
+    const article = articlesInfo.find(article => article.name === name);
+    if (article) {
+        article.comments.push({postedBy, text});
+        res.send(`The ${name} article now has a comment starting with "${text.substring(0,10)}..." by ${postedBy}.`)
+    } else {
+        res.send(`That article doesn't exist!`);
+    }
+});
+
+
 app.listen(listeningPort, () => {
     console.log(`Server is listening on port ${listeningPort}`);
 });
