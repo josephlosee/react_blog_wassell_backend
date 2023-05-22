bd67d9ac9ae6e71843958808a59206d3539b4575
Joe Losee <josephlosee@gmail.com>
2023-05-22 16:05:33 -0700

    Ch2.5 - Completed basic upvote endpoint.

--- src/server.js
+++ src/server.js
@@ -1,23 +1,38 @@
 import express from "express";
+
+let articlesInfo = [
+    {
+        name: 'learn-react',
+        upvotes: 0,
+    },
+    {
+        name: 'learn-node',
+        upvotes: 0,
+    },
+    {
+        name: 'mongodb',
+        upvotes: 0,
+    },
+]
+
 const listeningPort = 8000;
 
 const app = express();
 
 app.use(express.json()); // Middleware!
 
-
-
-app.post('/hello', (req, res)=>{
-    console.log(req.body);
-    res.send(`Hello, ${req.body.name}!`);
-});
-
-app.get('/hello/:name', (req, res) => {
-    // const name = req.params.name;
+app.put('/api/articles/:name/upvote', (req, res) => {
     const {name} = req.params;
-    res.send(`Hello, ${name}, how odd to include it as a URL Parameter!`);
-} )
-
+    const article = articlesInfo.find(article => article.name === name);
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes}  upvotes.`)
+    } else {
+        res.send(`That article doesn't exist!`);
+    }
+        
+    
+})
 app.listen(listeningPort, () => {
     console.log(`Server is listening on port ${listeningPort}`);
 });
