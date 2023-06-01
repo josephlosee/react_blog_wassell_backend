c11226fb17ea43ceed1dec034c5bcba376d5a999
Joe Losee <josephlosee@gmail.com>
2023-06-01 13:53:45 -0700

    Ch5.8 - Completed adding basic user auth to the get article endpoint
    Adds middleware for extracting user info
    Adds interface for enriching the request object
    Checks if user has already upvoted and adds a property

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import fs from 'fs';
 import admin from 'firebase-admin';
-import express from "express";
+import express, { Request } from "express";
 import { db, connectToDb } from "./db";
+import { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
+
+interface MyBlogRequest extends Request {
+    user?: DecodedIdToken
+}
 
 const listeningPort = 8000;
 
@@ -11,7 +16,21 @@ const credentials = JSON.parse(fs.readFileSync('../credentials.json').toString()
 admin.initializeApp({credential: admin.credential.cert(credentials)});
 
 const app = express();
+
 app.use(express.json()); // Middleware!
+
+app.use(async (req: MyBlogRequest, res, next) => {
+    const {authtoken } = req.headers;
+    if (authtoken){
+        try {
+            req.user = await admin.auth().verifyIdToken(authtoken as string);
+        } catch(e) {
+            res.sendStatus(400);
+        }
+        
+    }   
+    next(); 
+});
 
 app.get('/api/articles/:name', async (req, res) => {
     const {name} = req.params;
@@ -25,8 +44,10 @@ app.get('/api/articles/:name', async (req, res) => {
     }
 });
 
-app.put('/api/articles/:name/upvote', async (req, res) => {
+app.put('/api/articles/:name/upvote', async (req: MyBlogRequest, res) => {
     const {name} = req.params;
+    const {uid} = req.user;
+
 
     await db.collection('articles').updateOne({name}, {$inc: {upvotes: 1}}); 
     // I'm not familiar with mongodb so the query here is saying "find an article with this name, then increase ($inc) it's upvotes by 1"
@@ -35,6 +56,8 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
     
     
     if (article) {
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.include(uid);
         res.json(article);
     } else {
         res.send(`That article doesn't exist!`);
